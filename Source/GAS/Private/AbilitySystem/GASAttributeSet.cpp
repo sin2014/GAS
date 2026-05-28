9a73eb8ad7a2f2721d241aa65e510ab7545e856c
ZYZ <2214484515@qq.com>
2026-05-28 20:58:44 +0800

    1.制作幽灵进度条，在血量、法力变化之后延迟变化 2.PostGameplayEffectExecute中添加血量、法力限制，纠正属性最终变化

--- Source/GAS/Private/AbilitySystem/GASAttributeSet.cpp
+++ Source/GAS/Private/AbilitySystem/GASAttributeSet.cpp
@@ -86,6 +86,16 @@ void UGASAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbac
 	
 	FEffectProperties Props;
 	SetEffectProperties(Data, Props);
+	
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{ 
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}
+	if (Data.EvaluatedData.Attribute == GetManaAttribute())
+	{
+		SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
+	}
+	
 }
 
 void UGASAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
